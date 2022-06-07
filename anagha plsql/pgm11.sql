b322941b2e54af78fd87c05a5fc8ac2ddfc48f77
Anaghacs <anaghasasidharan38@gmail.com>
2022-06-07 14:49:41 +0530

    cursor

---
+++ anagha plsql/pgm11.sql
@@ -0,0 +1,33 @@
+
+set serveroutput on;
+declare
+n number;
+i number;
+flag number;
+ 
+FUNCTION prime(x in out number)
+RETURN boolean
+IS
+	flag boolean;
+begin
+i:=2;
+flag:=1;
+n:=&n;
+ 
+for i in 2..n/2
+loop
+if mod(n,i)=0
+then
+flag:=0;
+exit;
+end if;
+end loop;
+ 
+if flag=1
+then
+dbms_output.put_line('prime');
+else
+dbms_output.put_line('not prime');
+end if;
+end;
+/
